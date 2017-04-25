1a6f2caf8fd2d1cbfdde2a9d713f78862cd94854
bicoom <sm@technikum-wien.at>
2017-04-25 20:18:03 +0200

    added Task1 project

---
+++ tasks/Task1/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            return 0;
+        }
+    }
+}
