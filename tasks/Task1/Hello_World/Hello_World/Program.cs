acff87813e47718d2fc9575fcf2ed4f5e82b640c
voltae <ic17b096@technikum-wien.at>
2018-02-24 09:24:52 +0100

    Adding the Hello World Project

---
+++ tasks/Task1/Hello_World/Hello_World/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // initialize a new string
+            string aFriend = "Franz";
+            // concatenate the string with the text output 
+            Console.WriteLine("Hello " + aFriend);
+
+            // concatenate two strings together
+            string bFriend = "Marian";
+            bFriend += " und Helmuth"; 
+
+            // print out the length of the second string
+            Console.WriteLine($"Hello {aFriend} and {bFriend.Length}");
+
+            // replace the name 'Helmuth' with the name 'Angela'
+            bFriend = bFriend.Replace("Helmuth", "Angela");
+            // print out the result
+            Console.WriteLine(bFriend);
+        }
+    }
+}
